a09921a2e2118cc4715c835c971f664f40790caf
gregor <yuanke2016@gmail.com>
2019-09-24 20:25:21 +0800

    add dashboard

--- init.el
+++ init.el
@@ -6,11 +6,8 @@
 (require 'neotree)
 (require 'markdown-mode)
 (require 'all-the-icons)
-
-
-(setq inhibit-splash-screen t)
-
-(setq initial-scratch-message "Happy Hacking!")
+(require 'dashboard)
+(require 'use-package)
 
 (tool-bar-mode 0)
 
@@ -22,7 +19,7 @@
  '(markdown-command "pandoc -f markdown -t html -s --highlight-style espresso")
  '(package-selected-packages
    (quote
-    (all-the-icons neotree markdown-mode rainbow-delimiters dracula-theme))))
+    (dashboard all-the-icons neotree markdown-mode rainbow-delimiters dracula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -36,3 +33,17 @@
 (global-set-key [f8] 'neotree-toggle)
 
 (global-linum-mode t)
+
+(dashboard-setup-startup-hook)
+;; Or if you use use-package
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+
+(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+
+(setq dashboard-banner-logo-title "Happy Hacking!")
+
+(set-default 'truncate-lines t)
+
