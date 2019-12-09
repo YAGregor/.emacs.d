754e5ceda7e7e9fd85cef027dd70d89733143789
gregor <yuanke2016@gmail.com>
2019-12-09 12:21:30 +0800

    feat: add airline

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
 (require 'use-package)
 (require 'evil)
 (require 'dracula-theme)
+(require 'json-mode)
+(require 'rainbow-delimiters)
+(require 'airline-themes)
 
 (tool-bar-mode 0)
 
@@ -21,10 +24,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
  '(markdown-command "pandoc -f markdown -t html -s --highlight-style espresso")
  '(package-selected-packages
    (quote
-    (evil-anzu dashboard all-the-icons neotree markdown-mode rainbow-delimiters dracula-theme))))
+    (airline-themes json-mode evil-anzu dashboard all-the-icons neotree markdown-mode rainbow-delimiters dracula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,6 +39,7 @@
  )
 
 (load-theme 'dracula t)
+(load-theme 'airline-dark)
 
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (global-set-key [f8] 'neotree-toggle)
